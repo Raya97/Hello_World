b5ee1422bec55e0a5154a81867a98a5d81796a83
Raisa <vlasenkoraisa9725@gmail.com>
2023-05-01 19:51:46 +0200

    Changet HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,6 @@ package main.java;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println ("Hello GOIT World!");
+        System.out.println ("This is our second commit");
     }
 }
